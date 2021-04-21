a2b2d6f7c64b01ab6337228edbdf29f1cef0a0dd
LeonidScorp <fishchukl28@gmail.com>
2021-04-21 12:42:30 +0300

    Desktop pixel-perfecting

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,30 +1,42 @@
-
-var gulp    = require('gulp'),
-  sass    = require('gulp-sass'),
-  concat  = require('gulp-concat'),
-  uglify  = require('gulp-uglify');
-
-var paths = {
-  styles: {
-    src: 'scss/**/*.scss',
-    dest: 'css'
-  }
-};
-
-function styles() {
-  return gulp
-    .src(paths.styles.src, {
+var gulp  = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcss      = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer');
+  var paths = {
+    styles: {
+      src: 'scss/**/*.scss',
+      dest: 'css'
+    }
+  };
+function buildCss() {
+    return gulp.src(paths.styles.src, {
       sourcemaps: true
     })
-    .pipe(sass())
-    .pipe(gulp.dest(paths.styles.dest));
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer({ overrideBrowserslist: [
+                'Chrome >= 35',
+                'Firefox >= 38',
+                'Edge >= 12',
+                'Explorer >= 10',
+                'iOS >= 8',
+                'Safari >= 8',
+                'Android 2.3',
+                'Android >= 4',
+                'Opera >= 12']})]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css/'))
+        .pipe(cleanCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css/'))
 }
 
-function watch() {
-  gulp.watch(paths.styles.src, styles);
+function watcher() {
+    gulp.watch(['scss/*.scss'], gulp.series(buildCss));
 }
 
-var build = gulp.parallel(styles, watch);
-
-gulp.task(build);
-gulp.task('default', build);
+exports.watch = gulp.series(buildCss, watcher);
+exports.default = gulp.series(buildCss);
